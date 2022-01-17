afea5b94cf7e64d287dfd157320f216aa9846886
RiGun-K <1234suho@naver.com>
2022-01-17 16:41:52 +0900

    브런치 변경

--- ExGen/app.js
+++ ExGen/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-////////// 주소 할당 //////////
+////////// 주소 할당 ///////////
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var infoRouter = require('./routes/info');
