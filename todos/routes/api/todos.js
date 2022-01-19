3e47b7c1e27a92af20374b36d4e9300b750fd51b
RiGun-K <1234suho@naver.com>
2022-01-19 09:23:28 +0900

    todoList

---
+++ todos/routes/api/todos.js
@@ -0,0 +1,24 @@
+const expree = require('express');
+const router = express.Router();
+
+// 할 일 리스트 조회
+router.get('/', (req, res)=>{
+    const todos=[
+        {todoId:1, job:'JS Study'},
+        {todoId:2, job:'Workout'}
+    ];
+    req.json(todos);
+});
+
+// 할 일 추가
+router.post('/', (req, res)=>{
+    const {job} = req.body;
+    res.json({result:'ok'});
+});
+
+// 할 일 삭제
+router.delete('/:todoId', (req, res)=>{
+    const todoId = req.params.todoId;
+    res.json({result:'ok'});
+});
+module.exports=router;
