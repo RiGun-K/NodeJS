ca1c04adcdb1d0e01880efe88d660b224324abff
RiGun-K <1234suho@naver.com>
2022-01-20 16:59:28 +0900

    2022/01/20

--- ExGen/routes/users.js
+++ ExGen/routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 // /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   const obj = {message:'Hello'};
-//   res.json(obj);   // json = 스트링이 아니라 객체로 ( RestController 방식이므로 html 필요없음 )
+//   res.json(obj);   // json = 스트링이 아니라 객체로 ( RestController 방식이므로 html 필요없음! )
 // });
 
 
