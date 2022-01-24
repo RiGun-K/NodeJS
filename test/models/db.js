92fdd3debc59dbe89de155684cccc57ae1e115bc
RiGun-K <1234suho@naver.com>
2022-01-24 10:49:55 +0900

    CRUD

---
+++ test/models/db.js
@@ -0,0 +1,35 @@
+const mariadb = require('mariadb')
+
+// const createpool(config)
+
+// DB 설정
+const pool = mariadb.createPool({
+    host: 'localhost',
+    port: 3306,
+    database:'node',
+    user: 'RiGun',
+    password: '4238',
+    connectionLimit: 4
+});
+
+// DB 연결
+const getConnection = async()=>{
+    return await pool.getConnection();
+}
+
+// async await 
+exports.executeQuery = async(sql, params)=>{
+    let conn, rows=null;
+    try{
+        conn = await getConnection();
+        if(params)
+            rows = await conn.query(sql, params);
+        else 
+            rows = await conn.query(sql);
+    }catch(err) {
+        console.log(err);
+    }finally{
+        if(conn) conn.end();
+    }
+    return rows;
+}
--- test/models/lecture.js
+++ test/models/lecture.js
@@ -1,5 +1,8 @@
 const db = require('./db');
 
 exports.getLecture = async()=>{
-    
+   let sql = 'select 1.*, t.name from lecutre 1';
+   sql += ''
+   sql += ''
+   return await db.query(); 
 }
