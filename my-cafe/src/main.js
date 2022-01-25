2cfad3cab08369fec6b2776a9aade1680af9b568
RiGun-K <1234suho@naver.com>
2022-01-25 19:16:14 +0900

    axios 연동

--- my-cafe/src/main.js
+++ my-cafe/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 // router 선언
 import router from './router/router'
+import store from './store'
+
 
 Vue.config.productionTip = false
 
 new Vue({
   router, // router 추가
+  store:store,
   render: h => h(App),
 }).$mount('#app')
