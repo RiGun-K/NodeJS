95f188ea358b25d1b7d0c48a4b1a0746b95de674
RiGun-K <1234suho@naver.com>
2022-01-27 16:30:44 +0900

    Read and Create

---
+++ board/src/components/Create.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <input v-model="writer" placeholder="글쓴이">
+        <input v-model="title" placeholder="글쓴이">
+        <textarea v-model="content" placeholder="내용"/>
+    </div>
+</template>
+
+<script>
+import data from '@/data'
+
+export default {
+    name: 'Create',
+    data() {
+        return {
+            data: data,
+            writer: "",
+            title: "",
+            content: ""
+        }
+    }
+}
+</script>
--- board/src/components/Read.vue
+++ board/src/components/Read.vue
@@ -1,11 +1,37 @@
 <template>
    <div>
-       Read
+       <table>
+        <tr>
+            <td>글쓴이</td>
+            <td>제목</td>
+            <td>내용</td>
+        </tr>
+        <tr :key="item" v-for="item in data">   
+            <td>{{ item.writer }}</td>
+            <td>{{ item.title }}</td>
+            <td>{{ item.content }}</td>
+        </tr>
+       </table>
+       <button @click="write">글쓰기</button>
    </div> 
 </template>
 
 <script>
+import data from '@/data'   // data의 index.js의 배열을 불러오기 위해서 먼저 선언
+
 export default {
-    
+    name: 'Read',           // 이름 선언
+    data() {            
+        return {
+            data: data      // 리턴할 데이터 선언
+        }
+    },
+    methods: {
+        write() {
+            this.$router.push({     // URL 이동
+                path: 'create'
+            })
+        }
+    }
 }
 </script>
