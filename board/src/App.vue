d5cbd6cec05e5d00f3ac3806fed7c508d4f0cb8c
RiGun-K <1234suho@naver.com>
2022-01-27 15:32:12 +0900

    게시판 CRUD

--- board/src/App.vue
+++ board/src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <div id="app">
-    <Header />  <!-- Header 파일을 불러다 씀 -->
-    <div id="content" class="content">
-      <router-view></router-view>
-    </div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<!-- components에 Header를 찾아서 Header를 import해서 Header.vue를 띄운다 -->
-
 <script>
-import Header from './components/layout/Header.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Header,
+    HelloWorld
   }
 }
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
