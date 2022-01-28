2c74c439784b58a65250f53fabc504885b5afac9
RiGun-K <1234suho@naver.com>
2022-01-28 11:24:44 +0900

    Detail

--- board/src/router/index.js
+++ board/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 
 import Read from '@/components/Read'
 import Create from '@/components/Create'
+import Detail from '@/components/Detail'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/create',
       name: 'Create',
       component: Create
+    },
+    {
+      path: '/detail/:contentId',
+      name: 'Detail',
+      component: Detail
     }
   ]
 })
