b5550a7a4299f199dd04531c0b44c37ea3391d54
RiGun-K <1234suho@naver.com>
2022-02-03 10:42:16 +0900

    Vue와 bootstrap을 활용한 게시판만들기

--- bootstrap-board/src/App.vue
+++ bootstrap-board/src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <Header/>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    
+    <Header />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Header
+    Header,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- bootstrap-board/src/components/Board.vue
+++ bootstrap-board/src/components/Board.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
-    <b-table striped hover :items="items"></b-table>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+    <!-- fields 속성 넣기 (보여주고싶은 부분만 보이게) -->
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        items: [
-          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
-      }
+import data from '@/data'   // import로 데이터 불러오기 
+
+export default {
+  name: 'Board',
+  data() {
+    return {
+      fields: [
+        {
+          key: 'content_id',
+          label: '글번호',
+        },
+        {
+          key: 'title',
+          label: '제목',
+        },
+        {
+          key: 'created_at',
+          label: '등록일'
+        },
+      ],
+      items: data.Content
     }
   }
+}
 </script>
