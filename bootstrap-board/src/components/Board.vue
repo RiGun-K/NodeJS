bd4adee2fbfbaded4ec4ab985090c8dc076c3145
RiGun-K <1234suho@naver.com>
2022-02-07 19:28:32 +0900

    페이지네이션 구현

--- bootstrap-board/src/components/Board.vue
+++ bootstrap-board/src/components/Board.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <b-table striped hover :items="items" :fields="fields" @row-clicked="rowClick"></b-table>
+    <b-table striped hover :items="items" :per-page="perPage" :current-page="currentPage" :fields="fields" @row-clicked="rowClick"></b-table>
     <!-- fields 속성 넣기 (보여주고싶은 부분만 보이게) , rowClick 속성 넣기 ( 열 클릭시 이벤트 처리 ) -->
-    <b-pagination :per-page="perPage" :current-page="currentPage" v-model="currentPage" pills :total-rows="rows"></b-pagination>
+    <b-pagination :per-page="perPage" v-model="currentPage" align="center" :total-rows="rows"></b-pagination>
     <b-button pill variant="primary" @click="writeContent">글쓰기</b-button>
   </div>
 </template>
